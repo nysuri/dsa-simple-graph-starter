4e4995085cb89945dc374515499e5c3d51d90f26
Ali <a.ozcan98@hotmail.com>
2024-11-27 13:34:44 +0100

    refactor: added todo comments

--- src/SUGraph.java
+++ src/SUGraph.java
@@ -9,6 +9,8 @@ public class SUGraph<V extends Identifiable<ID>, ID> implements SimpleGraph<V, I
         // create a num graph under construction
         private final SUGraph<V,ID> suGraph = new SUGraph<>();
 
+
+        // TODO: implement builder methods..
         @Override
         public SimpleGraphBuilder<V, ID> addVertices(V... vertices) {
             return null;
@@ -30,37 +32,43 @@ public class SUGraph<V extends Identifiable<ID>, ID> implements SimpleGraph<V, I
         }
     }
 
-
+    // TODO
     @Override
     public boolean addVertex(V vertex) {
         return false;
     }
 
+    // TODO
     @Override
     public V getVertex(ID id) {
         return null;
     }
 
+    // TODO
     @Override
     public boolean addEdge(V vertex1, V vertex2) {
         return false;
     }
 
+    // TODO
     @Override
     public boolean addEdge(ID vertexId1, ID vertexId2) {
         return false;
     }
 
+    // TODO
     @Override
     public Collection<V> getVertices() {
         return List.of();
     }
 
+    // TODO
     @Override
     public Collection<V> getNeighbours(V vertex) {
         return List.of();
     }
 
+    // TODO
     @Override
     public Collection<V> getNeighbours(ID vertexId) {
         return List.of();
@@ -68,7 +76,7 @@ public class SUGraph<V extends Identifiable<ID>, ID> implements SimpleGraph<V, I
 
     @Override
     public int getNumVertices() {
-        return 0;
+        return vertices.size();
     }
 
     @Override
