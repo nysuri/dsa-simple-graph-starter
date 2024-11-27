ab88cb871d7936e7cb9c49ad07ed58f9e6af878d
Ali <a.ozcan98@hotmail.com>
2024-11-27 13:21:04 +0100

    feat: vertex struct

---
+++ src/Vertex.java
@@ -0,0 +1,17 @@
+public class Vertex implements Identifiable<String>, Comparable<Vertex> {
+    private final String name;
+
+    public Vertex(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getId() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Vertex o) {
+        return name.compareTo(o.name);
+    }
+}
