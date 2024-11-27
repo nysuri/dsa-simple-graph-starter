276f047968a6037bb939ad8b288dba768002c046
Ali <a.ozcan98@hotmail.com>
2024-11-27 13:22:31 +0100

    feat: start implementation of SUGraph

---
+++ src/SUGraph.java
@@ -0,0 +1,59 @@
+import java.util.Collection;
+import java.util.List;
+
+public class SUGraph<V extends Identifiable<ID>, ID> implements SimpleGraph<V, ID> {
+    @Override
+    public boolean addVertex(V vertex) {
+        return false;
+    }
+
+    @Override
+    public V getVertex(ID id) {
+        return null;
+    }
+
+    @Override
+    public boolean addEdge(V vertex1, V vertex2) {
+        return false;
+    }
+
+    @Override
+    public boolean addEdge(ID vertexId1, ID vertexId2) {
+        return false;
+    }
+
+    @Override
+    public Collection<V> getVertices() {
+        return List.of();
+    }
+
+    @Override
+    public Collection<V> getNeighbours(V vertex) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<V> getNeighbours(ID vertexId) {
+        return List.of();
+    }
+
+    @Override
+    public int getNumVertices() {
+        return 0;
+    }
+
+    @Override
+    public int getNumEdges() {
+        return 0;
+    }
+
+    @Override
+    public String getAdjacencyReport() {
+        return "";
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+}
